f8949b0f3c82d72fe4b67034af85a65ca420c548
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-09 14:59:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,7 @@ tableau.extensions.initializeAsync().then(() => {
 function refresh() {
     let dashboard = tableau.extensions.dashboardContent.dashboard;
     let selectedWorksheet = dashboard.worksheets.find(w => w.name === 'Sales');
+    selectedWorksheet.getDataSourcesAsync().then(dataSources => {
+        let selectedDataSource = dataSources.find(ds => ds.name === 'Orders');
+    })
 }
