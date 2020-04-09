0bc27f4477905a06bfbc4a5d7b30636726a7642f
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-09 14:57:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 tableau.extensions.initializeAsync().then(() => {
   console.log('I have been initialized!!')
 });
+
+function refresh() {
+    [`let dashboard = tableau.extensions.dashboardContent.dashboard;`](https://tableau.github.io/extensions-api/docs/index.html)
+}
+let selectedWorksheet = dashboard.worksheets.find(w => w.name === 'Sales');
