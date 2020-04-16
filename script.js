6d1ebd9cb734b7ef80069bfe07247f7eb787ce15
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-16 16:14:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function refresh() {
             const dashboard = tableau.extensions.dashboardContent.dashboard; 
             let dataSourceFetchPromises = []; 
             let dashboardDataSources = {};
-            let Rcount = DataTable.totalRowCount();
+            let Rcount = tableau.extensions.totalRowCount();
             dashboard.worksheets.forEach(function (worksheet) { 
                 dataSourceFetchPromises.push(worksheet.getDataSourcesAsync()); 
             }); 
