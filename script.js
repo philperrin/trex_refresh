5cc77ba0beb33819f0d9f5048a1204c055988791
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-16 13:36:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ tableau.extensions.initializeAsync().then(() => {
   console.log('I have been initialized!!') 
 }); 
 
-function refreshAllDataSources() {  
+function refresh() {  
             const dashboard = tableau.extensions.dashboardContent.dashboard; 
             let dataSourceFetchPromises = []; 
             let dashboardDataSources = {};
