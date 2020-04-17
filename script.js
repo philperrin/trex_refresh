f665b7cd79415f713bd7bd53d5fa3af28647333f
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-17 07:26:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -48,6 +48,20 @@ function refresh() {
                 })
             })
         })
+
+
+        tableau.extensions.dashboardContent.worksheets.find((w2) => w2.name === 'records').getUnderlyingDataAsync().then((dataTable2) => {
+            const field2 = dataTable2.columns.find((column2) => column2.fieldName === 'Order ID');
+            const list2 = [];
+            for (const row2 of dataTable2.data) {
+                list2.push(row2[field2.index].value);
+            }
+            const values2 = list2.filter((el, i, arr) => arr.indexOf(el) === i);
+            paragraph.textContent += '\r\n \r\nNew Order Count: ';
+            paragraph.textContent += values2.length;
+
+
+        })
     })
 }
 /*         
@@ -58,8 +72,6 @@ function refresh() {
      list2.push(row2[field2.index].value);
    }
    const values2 = list2.filter((el, i, arr) => arr.indexOf(el) === i);
-
-
    paragraph.textContent += '\r\n \r\nNew Order Count: ';
    paragraph.textContent += values2.length;
    */
