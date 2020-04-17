8a7047cb61faac1207f969f0b49128820d59f6dd
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-16 22:14:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,50 @@
-tableau.extensions.initializeAsync().then(() => { 
-  console.log('I have been initialized!!') 
-}); 
+tableau.extensions.initializeAsync().then(() => {
+    console.log('I have been initialized!!')
+});
 
 var values = [];
 var list = [];
+
 function refresh() {
-            const dashboard = tableau.extensions.dashboardContent.dashboard; 
-//            const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
-//            let dataSourceFetchPromises = []; 
-//            let dashboardDataSources = {};
-//            var values = [];
-//            var list = [];
-            
-dashboard.worksheets.find(w => w.name === "records").getUnderlyingDataAsync().then(dataTable => {
-  let field = dataTable.columns.find(column => column.fieldName === "Number of Records");
-  let list = [];
-  for (let row of dataTable.data) {
-    list.push(row[field.index].value);
-  }
-  let values = list.filter((el, i, arr) => arr.indexOf(el) === i);
-  console.log(values.length)
-  document.open();
-  document.write("Data Last Refreshed:<br>");
-  document.write(Date());
-  document.write("<br><button onclick='refresh()'>Update Data</button><br><br>");
-  document.write("Previous Row Count: ");
-  document.write(values.length);
-  document.close();
-});
-  
-/*            dashboard.worksheets.forEach(function (worksheet) { 
-               dataSourceFetchPromises.push(worksheet.getDataSourcesAsync()); 
-           }); 
-            Promise.all(dataSourceFetchPromises).then(function (fetchResults) { 
-                fetchResults.forEach(function (dataSourcesForWorksheet) { 
-                    dataSourcesForWorksheet.forEach(function (dataSource) { 
-                        if (!dashboardDataSources[dataSource.id]) { 
-                            dashboardDataSources[dataSource.id] = dataSource; 
-                            dataSource.refreshAsync(); 
-                        } 
+    const dashboard = tableau.extensions.dashboardContent.dashboard;
+    //            const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+    //            let dataSourceFetchPromises = []; 
+    //            let dashboardDataSources = {};
+    //            var values = [];
+    //            var list = [];
+
+    dashboard.worksheets.find(w => w.name === "Sales and Profit per person").getUnderlyingDataAsync().then(dataTable => {
+        let field = dataTable.columns.find(column => column.fieldName === "Sales");
+        let list = [];
+        for (let row of dataTable.data) {
+            list.push(row[field.index].value);
+        }
+        let values = list.filter((el, i, arr) => arr.indexOf(el) === i);
+        console.log(values.length)
+        document.open();
+        document.write("Data Last Refreshed:<br>");
+        document.write(Date());
+        document.write("<br><button onclick='refresh()'>Update Data</button><br><br>");
+        document.write("Previous Row Count: ");
+        document.write(values.length);
+        document.close();
+
+
+        /*            dashboard.worksheets.forEach(function (worksheet) { 
+                       dataSourceFetchPromises.push(worksheet.getDataSourcesAsync()); 
+                   }); 
+                    Promise.all(dataSourceFetchPromises).then(function (fetchResults) { 
+                        fetchResults.forEach(function (dataSourcesForWorksheet) { 
+                            dataSourcesForWorksheet.forEach(function (dataSource) { 
+                                if (!dashboardDataSources[dataSource.id]) { 
+                                    dashboardDataSources[dataSource.id] = dataSource; 
+                                    dataSource.refreshAsync(); 
+                                } 
+                            }); 
+                        }); 
                     }); 
-                }); 
-            }); 
-            
- 
-  document.open();
-  document.write("Data Last Refreshed:<br>");
-  document.write(Date());
-  document.write("<br><button onclick='refresh()'>Update Data</button><br><br>");
-  document.write("Previous Row Count: ");
-  document.write(values.length);
-  document.close();
-  */
-
- 
-} 
-
+                    
+         
+        */
+    });
+}
