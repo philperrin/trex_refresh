1eced25cffa01a32c23b4582fd168732509467a6
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-16 18:34:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function refresh() {
             let dataSourceFetchPromises = []; 
             let dashboardDataSources = {};
             var Rcount = [];
-            dtable.totalRowCount(Rcount);
+            dtable.totalRowCount();
             dashboard.worksheets.forEach(function (worksheet) { 
                 dataSourceFetchPromises.push(worksheet.getDataSourcesAsync()); 
             }); 
