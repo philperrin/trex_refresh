caa1670274e18e5ed4dcc0c32aefe3cdb941ee97
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-17 01:27:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
 tableau.extensions.initializeAsync().then(() => {
-    console.log('I have been initialized!!')
+  console.log('I have been initialized!!');
 });
 
-var values = [];
-var list = [];
-var paragraph = document.getElementById("p");
+const values = [];
+const list = [];
+const paragraph = document.getElementById('p');
 
 function refresh() {
-    const dashboard = tableau.extensions.dashboardContent.dashboard;
-    const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
-    let dataSourceFetchPromises = [];
-    let dashboardDataSources = {};
+  const { dashboard } = tableau.extensions.dashboardContent;
+  const { worksheets } = tableau.extensions.dashboardContent.dashboard;
+  const dataSourceFetchPromises = [];
+  const dashboardDataSources = {};
 
-    tableau.extensions.initializeAsync().then(() => {
-        console.log('Re-initialized')
+  tableau.extensions.initializeAsync().then(() => {
+    console.log('Re-initialized');
+  });
+  dashboard.worksheets.find((w) => w.name === 'records').getUnderlyingDataAsync().then((dataTable) => {
+    const field = dataTable.columns.find((column) => column.fieldName === 'Order ID');
+    const list = [];
+    for (const row of dataTable.data) {
+      list.push(row[field.index].value);
+    }
+    const values = list.filter((el, i, arr) => arr.indexOf(el) === i);
+    console.log(values.length);
+
+    paragraph.textContent = 'Data Last Refreshed: \r\n';
+    paragraph.textContent += Date();
+    paragraph.textContent += '\r\n \r\nPrevious Order Count: ';
+    paragraph.textContent += values.length;
+
+
+    dashboard.worksheets.forEach((worksheet) => {
+      dataSourceFetchPromises.push(worksheet.getDataSourcesAsync());
     });
-    dashboard.worksheets.find(w => w.name === "records").getUnderlyingDataAsync().then(dataTable => {
-        let field = dataTable.columns.find(column => column.fieldName === "Order ID");
-        let list = [];
-        for (let row of dataTable.data) {
-            list.push(row[field.index].value);
-        }
-        let values = list.filter((el, i, arr) => arr.indexOf(el) === i);
-        console.log(values.length)
-
-        paragraph.textContent = 'Data Last Refreshed: \r\n';
-        paragraph.textContent += Date();
-        paragraph.textContent += '\r\n \r\nPrevious Order Count: ';
-        paragraph.textContent += values.length;
-
-
-
-
-        dashboard.worksheets.forEach(function(worksheet) {
-            dataSourceFetchPromises.push(worksheet.getDataSourcesAsync());
+    Promise.all(dataSourceFetchPromises).then((fetchResults) => {
+      fetchResults.forEach((dataSourcesForWorksheet) => {
+        dataSourcesForWorksheet.forEach((dataSource) => {
+          if (!dashboardDataSources[dataSource.id]) {
+            dashboardDataSources[dataSource.id] = dataSource;
+            dataSource.refreshAsync();
+          }
         });
-        Promise.all(dataSourceFetchPromises).then(function(fetchResults) {
-            fetchResults.forEach(function(dataSourcesForWorksheet) {
-                dataSourcesForWorksheet.forEach(function(dataSource) {
-                    if (!dashboardDataSources[dataSource.id]) {
-                        dashboardDataSources[dataSource.id] = dataSource;
-                        dataSource.refreshAsync();
-                    }
-                });
-            });
-        });
-
-// repurpose rowcount code here for values2
-        paragraph.textContent += '\r\n \r\nNew Order Count: ';
-//        paragraph.textContent += values2.length;
-                
+      });
     });
+
+
+    dashboard.worksheets.find((w) => w.name === 'records').getUnderlyingDataAsync().then((dataTable) => {
+      const field2 = dataTable.columns.find((column) => column.fieldName === 'Order ID');
+      const list2 = [];
+      for (const row of dataTable.data) {
+        list2.push(row[field2.index].value);
+      }
+      const values2 = list2.filter((el, i, arr) => arr.indexOf(el) === i);
+
+
+      paragraph.textContent += '\r\n \r\nNew Order Count: ';
+      paragraph.textContent += values2.length;
+    });
+  });
 }
