e6b83d434cab80b680c8cf555d62c730487bc5e2
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-17 01:34:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -45,17 +45,17 @@ function refresh() {
     });
 
 
-    dashboard.worksheets.find((w) => w.name === 'records').getUnderlyingDataAsync().then((dataTable2) => {
-      const field2 = dataTable2.columns.find((column) => column.fieldName === 'Order ID');
-      const list2 = [];
-      for (const row of dataTable2.data) {
-        list2.push(row[field2.index].value);
+    dashboard.worksheets.find((w) => w.name === 'records').getUnderlyingDataAsync().then((dataTable) => {
+      const field = dataTable.columns.find((column) => column.fieldName === 'Order ID');
+      const list = [];
+      for (const row of dataTable.data) {
+        list.push(row[field.index].value);
       }
-      const values2 = list2.filter((el, i, arr) => arr.indexOf(el) === i);
+      const values = list.filter((el, i, arr) => arr.indexOf(el) === i);
 
 
       paragraph.textContent += '\r\n \r\nNew Order Count: ';
-      paragraph.textContent += values2.length;
+      paragraph.textContent += values.length;
     });
   });
 }
