432af9c6ead383c63e1bdd90e6d78ec6a4d6a849
philperrin <35937414+philperrin@users.noreply.github.com>
2020-04-17 00:15:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -21,11 +21,10 @@ function refresh() {
         let values = list.filter((el, i, arr) => arr.indexOf(el) === i);
         console.log(values.length)
         
-//        paragraph.textContent += "Data Last Refreshed:<br>"
-        paragraph.textContent += '<br>'
-        paragraph.textContent += Date();
-//        paragraph.textContent += "<br><br>Previous Order Count: ";
-//        paragraph.textContent += values.length;
+        paragraph.textContent = "Data Last Refreshed:<br>"
+        paragraph.textContent = Date();
+        paragraph.textContent = "<br><br>Previous Order Count: ";
+        paragraph.textContent = values.length;
         
 /*        
         document.open();
